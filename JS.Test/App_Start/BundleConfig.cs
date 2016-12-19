ab3e694ffd42d8dc05ff108fd332b2c0898767f4
kerryhuang <zihao317@gmail.com>
2016-12-19 11:35:49 +0800

    Dropzone 檔案上傳

--- JS.Test/App_Start/BundleConfig.cs
+++ JS.Test/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace JS.Test
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/dropzonescripts").Include(
+                     "~/Scripts/dropzone/dropzone.js"));
+
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +29,11 @@ namespace JS.Test
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/dropzonescss").Include(
+                     "~/Scripts/dropzone/css/basic.css",
+                     "~/Scripts/dropzone/css/dropzone.css"));
+            
         }
     }
 }
---
+++ JS.Test/Controllers/DropzoneController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JS.Test.Controllers
+{
+    public class DropzoneController : Controller
+    {
+        // GET: Dropzone
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            return View();
+        }
+    }
+}
