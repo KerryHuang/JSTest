f3e688fea59923b4d89cbde7df30063f80bfb470
kerryhuang <zihao317@gmail.com>
2016-12-20 14:29:52 +0800

    Materialize - 遵循Google Material Design實作的CSS framework

---
+++ JS.Test/Controllers/MaterializeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JS.Test.Controllers
+{
+    public class MaterializeController : Controller
+    {
+        // GET: Materialize
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
