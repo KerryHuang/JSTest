b60f2b6778845654d9f475f2048dfa193c7dcdfa
kerryhuang <zihao317@gmail.com>
2016-12-20 14:35:44 +0800

    screenfull.js - 全螢幕顯示就是這麼簡單

---
+++ JS.Test/Controllers/ScreenfullController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JS.Test.Controllers
+{
+    public class ScreenfullController : Controller
+    {
+        // GET: Screenfull
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
