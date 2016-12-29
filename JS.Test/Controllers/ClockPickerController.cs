ecb73d03b067f0a8f4b1f77428bbdd59d1f4c226
kerryhuang <zihao317@gmail.com>
2016-12-29 10:34:05 +0800

    ClockPicker - 更直覺的時間選擇器

---
+++ JS.Test/Controllers/ClockPickerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JS.Test.Controllers
+{
+    public class ClockPickerController : Controller
+    {
+        // GET: ClockPicker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
